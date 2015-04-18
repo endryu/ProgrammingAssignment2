fc8fe7a4ec8d22d8ac461578ae43ac2ae303df49
endryu <dashevskii@gmail.com>
2015-04-18 22:49:33 +0300

    file cachematrix.R is changed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<<-NULL
+    function()x
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    dat<-x()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    m<<-solve(dat,...)
+    m        
+    ## Return a matrix that is the inverse of 'x'
 }
